df3da29b8edc14356ad33bbc0aead921c98cd397
jonathan-schmitt-mx <jonathan.schmitt@mendix.com>
2023-05-14 21:43:47 -0600

    added websockets

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
-use std::{env};
+use std::{ env, sync::Arc };
 
 use axum::{
     routing::{ get, patch, post },
     http::StatusCode,
     Json,
     Router,
-    extract::{ Query, Path },
+    extract::{ Query, Path, ws::{ WebSocketUpgrade, WebSocket, Message }, State },
     response::{ IntoResponse, Response },
 };
 use tower_http::{ trace::TraceLayer, cors::CorsLayer, services::ServeDir, services::ServeFile };
 use dotenv::dotenv;
 use slam_app_rust_server::{ db::*, models::* };
+use tokio::{sync::broadcast };
+use futures::{sink::SinkExt, stream::StreamExt};
+
+#[derive(Clone)]
+struct AppState {
+    tx: broadcast::Sender<String>,
+}
 
 #[tokio::main]
 async fn main() {
@@ -20,6 +27,9 @@ async fn main() {
     let serve_dir = ServeDir::new("./build").not_found_service(
         ServeFile::new("./build/index.html")
     );
+
+    let (tx, _rx) = broadcast::channel(100);
+    let state = Arc::new(AppState { tx });
 
     let app = Router::new()
         .route("/data/room", get(get_rooms).post(post_room))
@@ -31,6 +41,8 @@ async fn main() {
         .route("/data/round/:id", get(get_round))
         .route("/data/participation/:id", patch(patch_participation))
         .route("/data/score", get(get_scores).post(post_score))
+        .route("/data/ws", get(websocket_handler))
+        .with_state(state)
         .fallback_service(serve_dir)
         .layer(CorsLayer::permissive())
         .layer(TraceLayer::new_for_http());
@@ -41,6 +53,39 @@ async fn main() {
         ::bind(&service_endpoint.parse().unwrap())
         .serve(app.into_make_service()).await
         .unwrap();
+}
+
+async fn websocket_handler(ws: WebSocketUpgrade, State(state): State<Arc<AppState>>) -> Response {
+    ws.on_upgrade(|socket| handle_socket(socket, state))
+}
+
+async fn handle_socket(socket: WebSocket, state: Arc<AppState>) {
+    let (mut sender, mut receiver) = socket.split();
+
+    
+    let mut rx = state.tx.subscribe();
+
+    let mut send_task = tokio::spawn(async move {
+        while let Ok(msg) = rx.recv().await {
+            // In any websocket error, break loop.
+            if sender.send(Message::Text(msg)).await.is_err() {
+                break;
+            }
+        }
+    });
+
+    let tx = state.tx.clone();
+
+    let mut recv_task = tokio::spawn(async move {
+        while let Some(Ok(Message::Text(text))) = receiver.next().await {
+            let _ = tx.send(text);
+        }
+    });
+
+    tokio::select! {
+        _ = (&mut send_task) => recv_task.abort(),
+        _ = (&mut recv_task) => send_task.abort(),
+    };
 }
 
 async fn get_rooms() -> (StatusCode, Json<Vec<Room>>) {
@@ -118,9 +163,9 @@ async fn get_scores(params: Query<ScoreFilter>) -> Response {
 
 async fn advance_room(Path(id): Path<String>, Json(payload): Json<Vec<Participant>>) -> Response {
     let result = create_next_round(&mut establish_connection(), &id, payload);
+
     return (StatusCode::CREATED, Json(result)).into_response();
 }
-
 
 async fn get_round(Path(id): Path<String>) -> Response {
     let result = retrieve_round(&mut establish_connection(), &id);
@@ -136,12 +181,11 @@ async fn patch_participation(
 }
 
 async fn current_room(Path(id): Path<String>) -> Response {
-    
     let room_result = retrieve_room(&mut establish_connection(), id.as_str());
     if let Some(round_id) = room_result.room.round_id_current {
-    let result = retrieve_round(&mut establish_connection(), &round_id);
-    return (StatusCode::OK, Json(result)).into_response();
+        let result = retrieve_round(&mut establish_connection(), &round_id);
+        return (StatusCode::OK, Json(result)).into_response();
     } else {
-        return (StatusCode::INTERNAL_SERVER_ERROR).into_response();
+        return StatusCode::INTERNAL_SERVER_ERROR.into_response();
     }
 }
